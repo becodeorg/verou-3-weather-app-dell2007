50664e0d63cb56966a133e281e66b782d3f2fbc7
Dericioso <dery.lantigua@gmail.com>
2022-01-25 15:44:09 +0100

    Add a new API with different data for forecast Fetch the city, lon and lat from the first API. Create a div to display time, city, temp & icon Create another div to display 5 days forecast with date, temperature & icon

--- The Weather App/script.js
+++ The Weather App/script.js
@@ -2,30 +2,72 @@ import Data from './config.js';
 
 const searchBar = document.getElementById('searchBar');
 const searchButton = document.getElementById('submitButton');
-const section = document.getElementsByTagName('section')[0];
+const todayForecast = document.getElementById('todayForecast');
+const forecast = document.getElementById('forecast');
+
+//Actual time
+// const actualDate = 
+
 
 searchButton.addEventListener('click', () => {
     let cityName = searchBar.value.toLowerCase();
 
-    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + Data.key)
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=' + Data.key)
         .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            for (let i = 0; i < 8; i++) {
-                const differentTime = document.createElement('p');
-                differentTime.innerHTML = data.list[i].dt_txt;
-                section.appendChild(differentTime);
-                const differentTemp = document.createElement('p');
-                section.appendChild(differentTemp);
-                differentTemp.innerHTML = Math.round(data.list[i].main.temp - 273.15);
-            }
-            for (let i = 8; i < 16; i++) {
-                const differentTime = document.createElement('p');
-                differentTime.innerHTML = data.list[i].dt_txt;
-                section.appendChild(differentTime);
-                const differentTemp = document.createElement('p');
-                section.appendChild(differentTemp);
-                differentTemp.innerHTML = Math.round(data.list[i].main.temp - 273.15);
-            }
+        .then(result => {
+            //Latitude & longitude to search on the second API
+            const lat = result.city.coord.lat;
+            const lon = result.city.coord.lon;
+
+            //City and country fetch from the first API
+            const city = document.createElement('h3');
+            city.setAttribute('id', 'city');
+            city.innerHTML = result.city.name + ', ' + result.city.country;
+
+
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,alert&units=metric&appid=' + Data.key)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+
+                    //Up to date forecast information box
+                    const actualTime = document.createElement('p');
+                    actualTime.setAttribute('id', 'timeNow');
+                    actualTime.innerHTML = new Date().toLocaleString().replace(',','');
+                    todayForecast.appendChild(actualTime);
+                    todayForecast.appendChild(city);
+                    const actualTemp = document.createElement('h3');
+                    actualTemp.innerHTML = Math.round(data.current.temp) + '°C';
+                    todayForecast.appendChild(actualTemp);
+                    const actualIcon = document.createElement('img');
+                    actualIcon.src = 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png';
+                    actualTemp.appendChild(actualIcon);
+
+                    for (let i = 0; i < 5; i++) {
+                        const icon = document.createElement('img');
+                        icon.src = 'http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png';
+                        forecast.appendChild(icon);
+                        const differentDay = document.createElement('tr');
+                        differentDay.innerHTML = new Date(data.daily[i].dt * 1000).toDateString();
+                        forecast.appendChild(differentDay);
+                        const differentTemp = document.createElement('tr');
+                        forecast.appendChild(differentTemp);
+                        differentTemp.innerHTML = Math.round(data.list[i].main.temp);
+                    }
+                })
+
+
+
+            // for (let i = 8; i < 16; i++) {
+            //     const icon = document.createElement('img');
+            //     icon.src = 'http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png';
+            //     forecast.appendChild(icon);
+            //     const differentTime = document.createElement('tr');
+            //     differentTime.innerHTML = data.list[i].dt_txt;
+            //     forecast.appendChild(differentTime);
+            //     const differentTemp = document.createElement('tr');
+            //     forecast.appendChild(differentTemp);
+            //     differentTemp.innerHTML = Math.round(data.list[i].main.temp);
+            // }
         })
 })
